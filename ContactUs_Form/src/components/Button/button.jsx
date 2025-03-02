7ea362aaa58a951860980c4e13fbf8107ba5d3f0
Pradeep Khatri <pradeepkhatri44@live.com>
2025-03-02 18:29:33 +0530

    last commit form
    last commit form

--- ContactUs_Form/src/components/Button/button.jsx
+++ ContactUs_Form/src/components/Button/button.jsx
@@ -1,11 +1,10 @@
 import { MdMessage } from "react-icons/md";
 import styles from "./button.module.css";
 const Button = (props) => {
-  console.log(props);
   return (
     <button className={styles.primary_btn}>
-      <MdMessage fontSize="24px" />
-      VIA SUPPORT CHAT
+      {props.icon}
+      {props.text}
     </button>
   );
 };
--- ContactUs_Form/src/components/ContactForm/ContactForm.jsx
+++ ContactUs_Form/src/components/ContactForm/ContactForm.jsx
@@ -1,10 +1,12 @@
 import styles from "./ContactForm.module.css";
+import { MdMessage } from "react-icons/md";
 
 const ContactForm = () => {
   return (
     <section className={styles.container}>
       <div className={styles.ContactForm}>
-        <button text="VIA SUPPORT CHAT" icon={<MdMessage />} />
+        <button text="VIA SUPPORT CHAT" icon={<MdMessage fontSize="24px" />} />
+        <button text="VIA CALL" icon={<MdMessage fontSize="24px" />} />
       </div>
       <div className={styles.contactImage}></div>
     </section>
