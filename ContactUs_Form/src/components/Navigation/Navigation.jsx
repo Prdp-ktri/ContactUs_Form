86df330b45ab690f130a9f99104dd991ae237bfa
Pradeep Khatri <pradeepkhatri44@live.com>
2025-03-03 19:03:30 +0530

    Update Navigation.jsx

--- ContactUs_Form/src/components/Navigation/Navigation.jsx
+++ ContactUs_Form/src/components/Navigation/Navigation.jsx
@@ -1,10 +1,10 @@
-import styles from './Navigation.module.css'
+import styles from "./Navigation.module.css";
 
 const Navigation = () => {
   return (
     <nav className={`${styles.navigation} container`}>
       <div className="logo">
-        <img src="/images/logo.png" alt="Do Some Coding Logo" />
+        <img src="/images/logo.png" alt="do some coding logo" />
       </div>
 
       <ul>
@@ -14,6 +14,6 @@ const Navigation = () => {
       </ul>
     </nav>
   );
-}
+};
 
 export default Navigation;
