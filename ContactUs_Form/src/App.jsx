cc0a7a31efc4334840f768adedaa7f2b9f30b8d8
Pradeep Khatri <pradeepkhatri44@live.com>
2025-03-03 19:05:10 +0530

    Update App.jsx

--- ContactUs_Form/src/App.jsx
+++ ContactUs_Form/src/App.jsx
@@ -1,19 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Navigation from "./components/Navigation/Navigation.jsx";
-import ContactHeader from "./components/ContactHeader/ContactHeader.jsx";
-import ContactForm from "./components/ContactForm/ContactForm.jsx";
-import Button from "./components/Button/button.jsx";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactHeader from "./components/ContactHeader/ContactHeader";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <div>
       <Navigation />
-      <ContactHeader />
-      <ContactForm />
-      <Button />
+      <main className="main_container">
+        <ContactHeader />
+        <ContactForm />
+      </main>
     </div>
   );
 }
